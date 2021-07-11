079e441f0b68b6709c4b431a4eaa65c8857a3add
ilzamafif <ilzamafif27@gmail.com>
2021-07-11 13:37:32 +0700

    tabel transactions

--- database/migrations/2021_07_11_063121_create_transactions_table.php
+++ database/migrations/2021_07_11_063121_create_transactions_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateTransactionDetailsTable extends Migration
+class CreateTransactionsTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,13 +13,13 @@ class CreateTransactionDetailsTable extends Migration
      */
     public function up()
     {
-        Schema::create('transaction_details', function (Blueprint $table) {
+        Schema::create('transactions', function (Blueprint $table) {
             $table->bigIncrements('id');
-            $table->integer('transactions_id');
-            $table->string('username');
-            $table->string('nationality');
-            $table->boolean('is_visa');
-            $table->date('doe_passport');
+            $table->integer('travel_packages_id');
+            $table->integer('users_id');
+            $table->integer('additional_visa');
+            $table->integer('transaction_total');
+            $table->string('transaction_status');
             $table->softDeletes();
             $table->timestamps();
         });
@@ -32,6 +32,6 @@ class CreateTransactionDetailsTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('transaction_details');
+        Schema::dropIfExists('transactions');
     }
 }
