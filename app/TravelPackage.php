26b99d29215a38a7300ed7c12e91355b435c3d5f
ilzamafif <ilzamafif27@gmail.com>
2021-07-12 08:01:38 +0700

    model travel package

--- app/TravelPackage.php
+++ app/TravelPackage.php
@@ -3,8 +3,11 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class TravelPackage extends Model
 {
-    //
+    protected $fillable = ['title', 'slug', 'location', 'about', 'featured_event', 'language', 'foods', 'departure_date', 'duration', 'type', 'price'];
+
+    protected $hidden = [];
 }
