3b100ec66ad64a6b887e3cbc7fbb1990c23e42cb
ilzamafif <ilzamafif27@gmail.com>
2021-07-13 13:46:49 +0700

    membuat model gallery

--- app/Gallery.php
+++ app/Gallery.php
@@ -2,9 +2,17 @@
 
 namespace App;
 
+use App\Http\Requests\Admin\TravelPackageRequest;
 use Illuminate\Database\Eloquent\Model;
 
 class Gallery extends Model
 {
-    //
+    protected $fillable = ['travel_packages_id', 'image'];
+
+    protected $hidden = [];
+
+    public function travel_package()
+    {
+        return $this->belongsTo(TravelPackage::class, 'travel_packages_id', 'id');
+    }
 }
