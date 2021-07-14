28580e25d8f61fdab77e7e68dac7003e1923e7ab
ilzamafif <ilzamafif27@gmail.com>
2021-07-14 12:13:45 +0700

    membuat statistikl dashboard

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -3,12 +3,22 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Transaction;
+use App\TravelPackage;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
 {
     public function index(Request $request)
     {
-        return view('pages.admin.dashboard');
+        return view(
+            'pages.admin.dashboard',
+            [
+                'pending' => Transaction::where('transaction_status', 'PENDING')->count(),
+                'success' => Transaction::where('transaction_status', 'SUCCESS')->count(),
+                'travel' =>  TravelPackage::count(),
+                'transaction' => Transaction::count(),
+            ]
+        );
     }
 }
