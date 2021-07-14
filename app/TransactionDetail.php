937202e45ef6bc0bda5d859f842a54fa8c66ab8e
ilzamafif <ilzamafif27@gmail.com>
2021-07-14 10:08:43 +0700

    model transaction detail

--- app/TransactionDetail.php
+++ app/TransactionDetail.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class TransactionDetail extends Model
 {
-    //
+    protected $fillable = [
+        'transaction_id', 'username', 'is_visa', 'doe_passport'
+    ];
+
+    protected $hidden = [];
+
+    public function transaction()
+    {
+        return $this->belongsTo(Transaction::class, 'transaction_id', 'id');
+    }
 }
