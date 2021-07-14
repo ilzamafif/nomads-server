970e10284010fc6a45a2f62b53fac997afbef5e9
ilzamafif <ilzamafif27@gmail.com>
2021-07-14 10:03:06 +0700

    membuat model transaction

--- app/Transaction.php
+++ app/Transaction.php
@@ -6,5 +6,24 @@ use Illuminate\Database\Eloquent\Model;
 
 class Transaction extends Model
 {
-    //
+    protected $fillable = [
+        'travel_packages_id', 'users_id', 'additional_visa', 'transaction_total', 'transaction_status'
+    ];
+
+    protected $hidden = [];
+
+    public function details()
+    {
+        return $this->HasMany(TransactionDetail::class, 'transaction', 'id');
+    }
+
+    public function travel_package()
+    {
+        return $this->belongsTo(TravelPackage::class, 'travel_packages_id', 'id');
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'users_id', 'id');
+    }
 }
